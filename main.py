5763e5d4657e89ef675cf53f33ced725422df6ba
Jaredhdev <jared.haby@gmail.com>
2023-07-04 12:37:18 +1000

    a few changes

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from overlay import compose_video
 from reddit import get_submission
 from tts import text_to_speech
 
-
-def create_story(url: str) -> None:
-    submission = get_submission(url)
-
-    text_to_speech(submission.title, './audio/title.mpeg')
-    words, times = text_to_speech(submission.selftext, './audio/output.mpeg')
-
-    create_title_img(submission.title, './images')
-    create_images(words, './images')
-
-    compose_video('./videos/Gameplay15.mp4', './audio/output.mpeg', './audio/title.mpeg', './images', times, './output')
-
-
 if __name__ == '__main__':
-    url = input('url: ')
+    title = ''
+    text = ''
 
-    create_story(url)
+    text_to_speech(title, './audio/title.mpeg')
+    words, times = text_to_speech(text, './audio/output.mpeg')
+
+    images_folder = './images'
+    create_title_img(title, images_folder)
+    create_images(words, images_folder)
+
+    compose_video('./videos/Gameplay15.mp4', './audio/output.mpeg', './audio/title.mpeg', images_folder, times, './output')
