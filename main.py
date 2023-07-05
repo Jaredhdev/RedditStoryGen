25c04d4bc9eaf44bd19c971a0ea586af2bf5138b
Jaredhdev <jared.haby@gmail.com>
2023-07-05 17:58:49 +1000

    heroku

--- main.py
+++ main.py
@@ -43,6 +43,3 @@ def getvideo(unique_id):
 
     return send_file(video_path, mimetype='video/mp4', as_attachment=True)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
