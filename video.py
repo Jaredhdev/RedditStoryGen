3761c3da505455bfd5d60fa112c2ae5e7a53e29d
Jaredhdev <jared.haby@gmail.com>
2023-07-06 14:33:24 +1000

    getting vid from gdrive

--- video.py
+++ video.py
@@ -1,10 +1,19 @@
 import os
-import gdown
+import boto3
 
 
 def download_video():
-    url = 'https://drive.google.com/uc?id=1weRn9yhOsq1Sf8t8lz6yg6Sy-ZOOj3o7'
-    output = '/tmp/minecraft15.mp4'
+    session = boto3.session.Session()
+    s3 = session.client('s3',
+                        region_name='us-west-2',
+                        endpoint_url='https://s3.amazonaws.com',
+                        aws_access_key_id='AKIAW5L5XMDNKVMXOPPA',
+                        aws_secret_access_key='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')
 
-    if not os.path.exists(output):
-        gdown.download(url, output, quiet=False)
+    filename = 'minecraft15.mp4'
+    bucket_name = 'reddit-story-gen'
+
+    path = os.path.join('/tmp', filename)
+
+    if not os.path.exists(path):
+        s3.download_file(bucket_name, filename, path)
