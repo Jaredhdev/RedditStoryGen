818d931d60879c6cbb0fbfc473482dc999e4bc3b
Jaredhdev <jared.haby@gmail.com>
2023-07-09 12:30:26 +1000

    celery

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import uuid
 import shutil
 import os
 
-from text import create_images, create_title_img
-from overlay import compose_video
-from tts import text_to_speech
-from video import download_video
+from tasks import process_video
 
 app = Flask(__name__)
 
@@ -18,24 +15,16 @@ def generate():
         title = data['title']
         text = data['text']
 
-        download_video()
         unique_id = str(uuid.uuid4())
-        dir_path = './' + unique_id
-        os.makedirs(dir_path)
 
-        text_to_speech(title, f'{dir_path}/title.mpeg')
-        words, times = text_to_speech(text, f'{dir_path}/text.mpeg')
-        create_title_img(title, dir_path)
-        create_images(words, dir_path)
-        compose_video(dir_path, times)
+        process_video.delay(title, text, unique_id)
 
         response = {
             'success': True,
-            'message': 'Video Generated',
-            'unique_id': unique_id,  # Send unique_id to user
-            'download_url': url_for('getvideo', unique_id=unique_id, _external=True)  # Sending back the full URL
+            'message': 'Video Generation Job Enqueued',
+            'job_id': unique_id
         }
-        print(response)
+
         return jsonify(response), 200
 
 
---
+++ tasks.py
@@ -0,0 +1,32 @@
+from celery import Celery
+import os
+
+from text import create_images, create_title_img
+from overlay import compose_video
+from tts import text_to_speech
+from video import download_video
+
+# Initialize Celery
+celery = Celery(__name__)
+
+redis_url = 'redis://:pa835a2983f9cf8e5e85c13198a0d3f4a1433518e2afec3ef88a9b0ab321b17e7@ec2-3-219-149-39.compute-1' \
+            '.amazonaws.com:24019'
+
+# Set Celery configuration to use Heroku Redis
+celery.conf.broker_url = redis_url
+celery.conf.result_backend = redis_url
+
+
+@celery.task
+def process_video(title: str, text: str, unique_id: str):
+    dir_path = './' + unique_id
+    os.makedirs(dir_path)
+
+    download_video()
+    text_to_speech(title, f'{dir_path}/title.mpeg')
+    words, times = text_to_speech(text, f'{dir_path}/text.mpeg')
+    create_title_img(title, dir_path)
+    create_images(words, dir_path)
+    compose_video(dir_path, times)
+
+    print('Video generation completed.')
