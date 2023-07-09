ea893d1a90d96779457fd0b645741ab3702b883d
Jaredhdev <jared.haby@gmail.com>
2023-07-09 11:07:02 +1000

    security

--- video.py
+++ video.py
@@ -1,14 +1,18 @@
 import os
 import boto3
+from dotenv import load_dotenv
 
 
 def download_video():
     session = boto3.session.Session()
+    load_dotenv()
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
     s3 = session.client('s3',
                         region_name='us-west-2',
                         endpoint_url='https://s3.us-west-2.amazonaws.com',
-                        aws_access_key_id='AKIAW5L5XMDNKVMXOPPA',
-                        aws_secret_access_key='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')
+                        aws_access_key_id=aws_access_key_id,
+                        aws_secret_access_key=aws_secret_access_key)
 
     filename = 'minecraft15.mp4'
     bucket_name = 'reddit-story-gen'
