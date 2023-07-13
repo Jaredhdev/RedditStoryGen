3047f7736a7c50dbac4f49a713301bf2a1c92ae9
Jaredhdev <jared.haby@gmail.com>
2023-07-13 12:21:20 +1000

    nearly integrated with frontend

--- tts.py
+++ tts.py
@@ -5,23 +5,22 @@ import os
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.getenv("API_KEY_NAME")
-authenticator = IAMAuthenticator(api_key)
-
-access_token = authenticator.token_manager.get_token()
-
-
-ws_url = "wss://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/93188035-3405-41a7-8f3d-fb9072efb9ad"
-
-voice = "en-US_MichaelExpressive"
-rate = 1
-
-wsURI = f"{ws_url}/v1/synthesize?access_token={access_token}&voice={voice}&rate_percentage={rate}"
-
 
 def text_to_speech(text: str, out_path: str) -> tuple:
-    ws = websocket.create_connection(wsURI, sslopt={"cert_reqs": ssl.CERT_NONE})
+    load_dotenv()
+    api_key = os.getenv("API_KEY_NAME")
+    authenticator = IAMAuthenticator(api_key)
+
+    access_token = authenticator.token_manager.get_token()
+
+    ws_url = "wss://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/93188035-3405-41a7-8f3d-fb9072efb9ad"
+
+    voice = "en-US_MichaelExpressive"
+    rate = 1
+
+    ws_uri = f"{ws_url}/v1/synthesize?access_token={access_token}&voice={voice}&rate_percentage={rate}"
+
+    ws = websocket.create_connection(ws_uri, sslopt={"cert_reqs": ssl.CERT_NONE})
 
     message = {
         'text': text,
